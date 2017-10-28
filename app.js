ecf0265a1aa3f7c96d736d6bf896350fc8a642ba
Jessica Roque <jroque.dev@gmail.com>
2017-10-27 17:00:24 -0700

    Completed Checkpoint 8: Simple To-Do: DOM Scripting - Events Part Two. Refactored previous checkpoint's code to account for state and UI.

--- app.js
+++ app.js
@@ -1,36 +1,51 @@
 function onReady() {
+  const toDos = [];
   const addToDoForm = document.getElementById('addToDoForm');
-  const newToDoText = document.getElementById('newToDoText');
-  const toDoList = document.getElementById('toDoList');
 
-  addToDoForm.addEventListener('submit', () => {
-    event.preventDefault();
 
-    // get the text
-    let title = newToDoText.value;
+  function createNewToDo() {
+    const newToDoText = document.getElementById('newToDoText');
+    if (!newToDoText) { return };
 
-    // create a new li
-    let newLi = document.createElement('li');
+    toDos.push({
+      title: newToDoText.value,
+      complete: false
+    });
 
-    // create a new input
-    let checkbox = document.createElement('input');
-
-    // set the input's type to checkbox
-    checkbox.type = "checkbox";
-
-    // set the title
-    newLi.textContent = title;
-
-    // attach the checkbox to the li
-    newLi.appendChild(checkbox);
-
-    // attach the li to the ul
-    toDoList.appendChild(newLi);
-
-    //empty the input
     newToDoText.value = '';
 
-  });
+    renderTheUI(toDos);
+  }
+
+  function renderTheUI(toDos) {
+    const todoList = document.getElementById('toDoList');
+
+    toDoList.textContent = '';
+
+    toDos.forEach(function(toDo) {
+      const newLi = document.createElement('li');
+      const checkbox = document.createElement('input');
+      checkbox.type = "checkbox";
+
+      newLi.textContent = toDo.title;
+
+      todoList.appendChild(newLi);
+      newLi.appendChild(checkbox);
+
+
+    });
+}
+
+//Why do we now need the word "event" after submit?
+//Didn't need this on the last assignment
+  addToDoForm.addEventListener('submit', event => {
+    event.preventDefault();
+    createNewToDo();
+});
+
+renderTheUI(toDos);
+
+
  }
 
 window.onload = function() {
